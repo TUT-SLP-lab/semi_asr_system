299cc8354c0d909088956869ac7f70794ac4ad74
kinouchi takahiro <drums.radwimps203@gmail.com>
2022-08-16 18:34:08 +0900

    [ADD] FastAPIの作成(docker container試運転はまだ)

--- src/run_asr_server.py
+++ src/run_asr_server.py
@@ -1,4 +1,9 @@
-from asr_system.view.fastapi_server import FastAPIServer
+from asr_system.view.fastapi_server import app
+from common.constant import ASR_SYSTEM_IP, ASR_SYSTEM_PORT
 
+import uvicorn
 
-FastAPIServer.run()
+if __name__ == "__main__":
+
+    uvicorn.run(app, host=ASR_SYSTEM_IP, port=ASR_SYSTEM_PORT)
+
