45c9da7a3b2830980e4054a40f9f61bbcd5564f5
Hansyo <hansyo.souti.1999@gmail.com>
2022-08-18 18:03:19 +0900

    [Update] 録音に関する諸機能が完成
    post処理のときのエラーハンドリングだけやったほうが良いかも。

--- src/recording/main.py
+++ src/recording/main.py
@@ -8,18 +8,22 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import requests
 import shutil
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def postQueueServer(post_id: ObjectId) -> None:
-    URL = 'http://172.0.0.1:6000/api/id'  # TODO: get server info from dotenv
-    payload = {'_id': post_id}
-    requests.post(URL, json=payload)
+    # TODO: get server ip from dotenv
+    url = f'http://{os.getenv("DISPATCHER_IP")}:{int(os.getenv("DISPATCHER_PORT"))}/api/id'
+    payload = {'_id': str(post_id)}
+    requests.post(url, json=payload)
 
 
 def registData(attribute: str, audio_path: str) -> None:
-    client = MongoClient()  # TODO: get client info from dotenv
-    db = client["asr_queue"]  # TODO: same above
-    collection = db["queue"]  # TODO: same above
+    client = MongoClient(os.getenv("MONGO_DB_IP"), int(os.getenv("MONGO_DB_PORT")))  # TODO: get client info from dotenv
+    db = client[os.getenv("MONGO_DB_NAME")]
+    collection = db[os.getenv("MONGO_COLLECTION_NAME")]
     post = {
         "attribute": attribute,
         "audio_path": audio_path,
@@ -32,20 +36,22 @@ def registData(attribute: str, audio_path: str) -> None:
 
 class Recorder:
     def __init__(self):
-        # TODO: deviceはdotenvで管理したい
         self.record = None
-        self.tmpdir = os.path.join(os.path.abspath(os.path.curdir), "record-data")
+        self.tmpdir = os.path.join(
+            os.path.abspath(os.path.curdir), "record-data")
         self.cmd = "bash recording.sh"
+        self.output_dir = os.path.abspath(os.getenv("AUDIO_OUTPUT_DIR"))
+        os.makedirs(self.output_dir, exist_ok=True)
         print(self.cmd)
 
     def start(self, presenter: str):
         if self.record is None:
             self.presenter = presenter
             self.attribute = f"{datetime.date.today()}_{presenter}"
-            self.tmp_file_path = os.path.join(self.tmpdir, f"{self.attribute}.wav")
-            # TODO: dotenv
-            # os.makedirs(self.output_dir, exist_ok=True)
-            # self.output_file_path = os.path.join(self.output_dir, f"{self.attribute}.wav")
+            self.tmp_file_path = os.path.join(
+                self.tmpdir, f"{self.attribute}.wav")
+            self.output_file_path = os.path.join(
+                self.output_dir, f"{self.attribute}.wav")
 
             # subprocessで録音処理を実行
             self.record = subprocess.Popen(f"exec {self.cmd}", shell=True)
@@ -55,10 +61,8 @@ class Recorder:
             self.record.kill()  # 録音を停止
             # recording.sh内でtmp.wavに音声を書き込んでいるので、
             # 正式な形にrenameする必要がある
-            # TODO: dotenvから設定を読み出して、そちらに移動させる
-            os.rename(os.path.join(self.tmpdir, "tmp.wav"), self.tmp_file_path)
-            # shutil.move(os.path.join(self.tmpdir, "tmp.wav"),
-            #             self.output_file_path)
+            shutil.move(os.path.join(self.tmpdir, "tmp.wav"),
+                        self.output_file_path)
             # DBに登録
             registData(attribute=self.attribute, audio_path=self.tmp_file_path)
 
@@ -93,7 +97,8 @@ class ThreadManager:
 def main():
     st.thread_manager = ThreadManager()
 
-    text_presenter = st.text_input("Presenter", key="presenter", disabled=st.thread_manager.is_running())
+    text_presenter = st.text_input(
+        "Presenter", key="presenter", disabled=st.thread_manager.is_running())
 
     # 録音を制御する部分
     if st.button("Start Recording", disabled=st.thread_manager.is_running()):
