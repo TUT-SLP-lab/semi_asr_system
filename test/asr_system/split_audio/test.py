9d1bc06d223b8f0c84964cfb62f09be127f13e32
TaMikawa <mikawa.tamon.fj@tut.jp>
2022-08-18 16:45:12 +0900

    [Update] split_audio.pyを更新

--- test/asr_system/split_audio/test.py
+++ test/asr_system/split_audio/test.py
@@ -1,1 +1,10 @@
+from src.asr_system.service.split_audio import *
+
 """split audioの単体テスト"""
+sox_com = Sox_Command(
+        in_format_op='-V3',
+        out_format_op=None,
+        silence_params=[1, 0.2, 0.2]*2
+)
+saudio = SplitAudio(sox_com)
+saudio.split('input.wav', 'output.wav')
