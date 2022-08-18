83c0edef0180705dcfa12f10508aab00b698daef
keigoj <hojojo2000@gmail.com>
2022-08-18 18:33:57 +0900

    言語モデルを使用するように設定

--- src/asr_system/service/asr_inference.py
+++ src/asr_system/service/asr_inference.py
@@ -3,8 +3,8 @@ import soundfile
 from espnet2.bin.asr_inference import Speech2Text
 
 class ASRInference:
-    def __init__(self, config_path: str, model_path: str):
-        self.s2t = Speech2Text(config_path, model_path)
+    def __init__(self, asr_config: str, asr_model: str, lm_config: str, lm_model: str):
+        self.s2t = Speech2Text(asr_train_config=asr_config, asr_model_file=asr_model, lm_train_config=lm_config, lm_file=lm_model)
 
     def speech2text(self, audio_path: list):
         hyp = []
--- test/asr_system/asr_inference/test.py
+++ test/asr_system/asr_inference/test.py
@@ -7,11 +7,13 @@ from asr_system.service.asr_inference import ASRInference
 
 if __name__=="__main__":
     wav_path = ['data/A01M0041_1.wav', 'data/A01M0041_2.wav', 'data/A01M0041_3.wav', 'data/A01M0041_4.wav', 'data/A01M0041_5.wav']
-    config_path = 'data/config.yaml'
-    model_path = 'data/40epoch.pth'
+    asr_config = 'data/config.yaml'
+    asr_model = 'data/40epoch.pth'
+    lm_config = 'data/config_lm.yaml'
+    lm_model = 'data/40epoch_lm.pth'
     file_name = 'test.txt'
 
-    asr = ASRInference(config_path, model_path)
+    asr = ASRInference(asr_config, asr_model, lm_config, lm_model)
     hyp = asr.speech2text(wav_path)
     print(hyp)
     asr.output_file(hyp, file_name)
