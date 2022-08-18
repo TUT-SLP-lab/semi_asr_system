bfdf2466cf653de91adc25944011ffc68604d45d
keigoj <hojojo2000@gmail.com>
2022-08-18 14:07:43 +0900

    音声認識機能の追加

--- src/asr_system/service/asr_inference.py
+++ src/asr_system/service/asr_inference.py
@@ -1,6 +1,21 @@
-def ASRInference():
-    def __init__(self):
-        pass
+import espnet2
+from espnet2.bin.asr_inference import Speech2Text
+import soundfile
 
-    def speech2text():
-        pass
+class ASRInference:
+    def __init__(self):
+        self.hyp = []
+
+    def speech2text(self, wav_path, config_path, model_path):
+        s2t = Speech2Text(config_path, model_path)
+        for w in wav_path:
+            #print(w)
+            audio, rate = soundfile.read(w)
+            result = s2t(audio)
+            self.hyp.append((result[0])[0])
+        return self.hyp
+    
+    def output_file(self, fname):
+        h = '\n'.join(self.hyp)
+        with open(fname, mode='w') as f:
+            f.write(h)
--- test/asr_system/asr_inference/test.py
+++ test/asr_system/asr_inference/test.py
@@ -1,1 +1,21 @@
 """ASR inference スクリプトの単体テスト"""
+
+import sys
+import os
+
+sys.path.append(f"/home/hojokeigo/semi_asr_sysystem_2/src/")
+
+print(sys.path)
+
+from asr_system.service.asr_inference import ASRInference
+
+if __name__=="__main__":
+    wav_path = ['data/A01M0041_1.wav', 'data/A01M0041_2.wav', 'data/A01M0041_3.wav', 'data/A01M0041_4.wav', 'data/A01M0041_5.wav']
+    config_path = 'data/config.yaml'
+    model_path = 'data/40epoch.pth'
+    file_ = 'test.txt'
+
+    asr = ASRInference()
+    hyp = asr.speech2text(wav_path, config_path, model_path)
+    print(hyp)
+    asr.output_file(file_name)
