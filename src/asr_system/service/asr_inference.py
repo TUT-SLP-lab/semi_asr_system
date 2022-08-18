b256a51590cb1c15656eb05699cd5dc2df643ff6
keigoj <hojojo2000@gmail.com>
2022-08-18 15:55:27 +0900

    音声認識機能の追加

--- src/asr_system/service/asr_inference.py
+++ src/asr_system/service/asr_inference.py
@@ -1,21 +1,20 @@
 import espnet2
-from espnet2.bin.asr_inference import Speech2Text
 import soundfile
+from espnet2.bin.asr_inference import Speech2Text
 
 class ASRInference:
-    def __init__(self):
-        self.hyp = []
+    def __init__(self, config_path: str, model_path: str):
+        self.s2t = Speech2Text(config_path, model_path)
 
-    def speech2text(self, wav_path, config_path, model_path):
-        s2t = Speech2Text(config_path, model_path)
-        for w in wav_path:
-            #print(w)
-            audio, rate = soundfile.read(w)
-            result = s2t(audio)
-            self.hyp.append((result[0])[0])
-        return self.hyp
+    def speech2text(self, audio_path: list):
+        hyp = []
+        for ad in audio_path:
+            audio, rate = soundfile.read(ad)
+            result = self.s2t(audio)
+            hyp.append((result[0])[0])
+        return hyp
     
-    def output_file(self, fname):
-        h = '\n'.join(self.hyp)
+    def output_file(self, hyp: list, fname: str):
+        h = '\n'.join(hyp)
         with open(fname, mode='w') as f:
             f.write(h)
--- test/asr_system/asr_inference/test.py
+++ test/asr_system/asr_inference/test.py
@@ -2,20 +2,16 @@
 
 import sys
 import os
-
-sys.path.append(f"/home/hojokeigo/semi_asr_sysystem_2/src/")
-
-print(sys.path)
-
+import _path
 from asr_system.service.asr_inference import ASRInference
 
 if __name__=="__main__":
     wav_path = ['data/A01M0041_1.wav', 'data/A01M0041_2.wav', 'data/A01M0041_3.wav', 'data/A01M0041_4.wav', 'data/A01M0041_5.wav']
     config_path = 'data/config.yaml'
     model_path = 'data/40epoch.pth'
-    file_ = 'test.txt'
+    file_name = 'test.txt'
 
-    asr = ASRInference()
-    hyp = asr.speech2text(wav_path, config_path, model_path)
+    asr = ASRInference(config_path, model_path)
+    hyp = asr.speech2text(wav_path)
     print(hyp)
-    asr.output_file(file_name)
+    asr.output_file(hyp, file_name)
