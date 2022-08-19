1940ebf0906e48a26de057396b7af90e1378410e
Hansy <hansyo.souti.1999@gmail.com>
2022-08-20 00:10:28 +0900

    Revert "Feature/client dispatcher"

--- src/dispatcher/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Audio(BaseModel):
-    attribute: str
-    audio_path: str
-    text_path: str
-    status: str
--- test/asr_system/split_audio/test.py
+++
@@ -1,7 +0,0 @@
-from asr_system.service.split_audio import *
-import _path
-
-"""split audioの単体テスト"""
-sox_com = Sox_Command(in_format_op="-V3", out_format_op=None, silence_params=[1, 0.2, 0.2] * 2)
-saudio = SplitAudio(sox_com)
-saudio.split("input.wav", "output.wav")
