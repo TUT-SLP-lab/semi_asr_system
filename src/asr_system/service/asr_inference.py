0c8a7b3bbef456247a41c57f731b9427a0ebe3cc
Hansyo <hosoi.sota.zw@tut.jp>
2023-08-23 21:20:51 +0900

    refactor: asr_inference.py

--- src/asr_system/service/asr_inference.py
+++ src/asr_system/service/asr_inference.py
@@ -1,7 +1,8 @@
 import torch
 import soundfile
-import resampy
+import subprocess
 import nemo.collections.asr as nemo_asr
+
 
 class ASRInference:
     def __init__(self, asr_train_config: str, asr_model_file: str, lm_train_config: str, lm_file: str):
@@ -19,19 +20,11 @@ class ASRInference:
 
     def speech2text(self, audio_path: str):
         audio, rate = soundfile.read(audio_path)
-        # 2チャンネルの場合、一つに変換
-        if audio.ndim == 2:
-            audio = audio[:, 0]
         # 長さ0の場合は無視
         if len(audio) == 0:
             return '[emp]'
         else:
-            print(f"audio dim{audio.ndim}")
-            # 16000kHzに変換
-            audio = resampy.resample(audio, rate, 16000)
-            print(f"sound length :{len(audio)}")
-            result = self.s2t(audio)
-            return (result[0])[0]
+            return self.s2t.transcribe([audio_path])[0]
 
     def output_file(self, hyp: list, fname: str):
         h = "\n".join(hyp)
